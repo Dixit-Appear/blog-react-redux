24017a801273169c645ed8f65f42f2f5ed5f5882
Edoardo Olivieri <diddo.42@hotmail.it>
2019-06-12 14:20:19 +0100

    webpack error

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,6 +5,20 @@ export const FETCH_POSTS = 'FETCH_POSTS';
 export const FETCH_POST = 'FETCH_POST';
 export const POST_CREATED = 'POST_CREATED';
 
+export function createPost(body, callback) {
+  const request = fetch(`${ROOT_URL}?key=${API_KEY}`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(body)
+  }).then(response => response.json())
+    .then(callback);
+
+  return {
+    type: POST_CREATED,
+    payload: request
+  };
+}
+
 export function fetchPosts() {
   const promise = fetch(`${ROOT_URL}?key=${API_KEY}`)
     .then(response => response.json());
@@ -22,20 +36,5 @@ export function fetchPost(id) {
   return {
     type: FETCH_POST,
     payload: promise
-  };
-}
-
-
-export function createPost(body, callback) {
-  const request = fetch(`${ROOT_URL}?key=${API_KEY}`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(body)
-  }).then(response => response.json())
-    .then(callback);
-
-  return {
-    type: POST_CREATED,
-    payload: request
   };
 }
